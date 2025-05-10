f5d95da3a5ca13edd662f77dc0d008e37fd49f56
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-05-10 16:17:30 +0530

    Quick fix setup

---
+++ shopseed/src/main/java/com/shopseed/ShopSeedApplication.java
@@ -0,0 +1,11 @@
+package com.shopseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSeedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSeedApplication.class, args);
+    }
+}
